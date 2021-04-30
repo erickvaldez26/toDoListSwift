105e79782265513b76263439bf2f941738a931d6
Erick Valdez <67769889+noks1i@users.noreply.github.com>
2021-04-29 22:08:38 -0500

    Initial Commit

---
+++ OurToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OurToDoList
+//
+//  Created by Erick on 29/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
